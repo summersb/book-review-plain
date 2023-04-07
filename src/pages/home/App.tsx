0705c8c397c949c75fc33020898023527234e12b
Bert W Summers <summersb@moesol.com>
2023-04-06 20:05:33 -0700

    Update firestore rules. added react query debug

--- src/pages/home/App.tsx
+++ src/pages/home/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import {AuthProvider} from '../../contexts/AuthProvider'
 import Login from '../login/Login'
 import Signup from '../signup/Signup'
@@ -40,6 +41,7 @@ function App({}: AppProps) {
             </Routes>
           </main>
         </AuthProvider>
+        <ReactQueryDevtools initialIsOpen={false}/>
       </QueryClientProvider>
     </BrowserRouter>
   )
